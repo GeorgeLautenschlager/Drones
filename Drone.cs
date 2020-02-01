201acceb11e1d33ded6a0bc91df321bbdf6919d7
George Lautenschlager <georgelautenschlager@gmail.com>
2020-01-31 23:43:18 -0500

    Add project files.

---
+++ Drone.cs
@@ -0,0 +1,57 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System;
+using VRage.Collections;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRage.Game;
+using VRage;
+using VRageMath;
+
+namespace IngameScript
+{
+    partial class Program
+    {
+        /*
+         * This is the entry point and ultimate container of all drone logic. Behaviours may live elsewhere, and actions will
+         * probably live in statis utility classes, but messages passed to or from a drone do so through the interface of this class.
+        */
+        public class Drone
+        {
+            private Program program;
+            private Role[] roles;
+
+            public Drone(Program program, Role[] roles)
+            {
+                this.program = program;
+                this.roles = roles;
+
+                string[] roleNames = new string[roles.Length];
+                roleNames = roles.Select(role => role.ToString()).ToArray();
+
+
+                program.Echo($"Initializing drone with roles: {String.Join(",", roleNames)}");
+            }
+
+            public void Act()
+            {
+                Log("Cogito Ergo Sum");
+                // ask my roles what to do
+            }
+
+            private void Log(string text)
+            {
+                this.program.Echo(text);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System;
+using VRage.Collections;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRage.Game;
+using VRage;
+using VRageMath;
+
+namespace IngameScript
+{
+    partial class Program : MyGridProgram
+    {
+        Drone drone;
+        public Program()
+        {
+            Runtime.UpdateFrequency = UpdateFrequency.Update100;
+
+            Role[] roles = new Role[1]{ new Miner() };
+
+            string[] roleNames = new string[roles.Length];
+            roleNames = roles.Select(role => role.ToString()).ToArray();
+
+            Echo($"Building drone with roles: {String.Join(",", roleNames)}");
+
+            drone = new Drone(this, roles);
+        }
+
+        public void Save()
+        {
+            // Called when the program needs to save its state. Use
+            // this method to save your state to the Storage field
+            // or some other means. 
+            // 
+            // This method is optional and can be removed if not
+            // needed.
+        }
+
+        public void Main(string argument, UpdateType updateSource)
+        {
+            Echo("Are you alive?");
+            drone.Act();
+        }
+    }
+}
